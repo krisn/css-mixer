36994e01682bec66fbadf18b825465b2b7694b06
Kris Nyunt <kris.nyunt@gmail.com>
2017-11-21 21:41:58 +1000

    Building now

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,7 +1,7 @@
 /**
  * css-mixer
  *
- * Copyright © 2016 Kris Nyunt. All rights reserved.
+ * Copyright © 2018 Kris Nyunt. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
